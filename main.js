1fe5ae2a1dbb37005783c075a5297f3525186f01
juan990521 <juanscastro@unicesar.edu.co>
2023-12-23 05:23:54 -0500

    monatada de la pagina con efectos javascript se agrego la clase inactive y tambien se creo los productos por medio de javaScript

--- main.js
+++ main.js
@@ -4,14 +4,123 @@ const menu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const iconHamburguesa = document.querySelector('.menu');
 
+const  menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+const cardsContainer = document.querySelector('.cards-container');
+
+
 li.addEventListener('click', toggleDesktopMenu);
 iconHamburguesa.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
+    //const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    
+    // if(!isMobileMenuClosed) {
+    //     mobileMenu.classList.add('inactive')
+    // }
+
+
         menu.classList.toggle('inactive');
 
 }
 
 function toogleMobileMenu() {
+    
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMenuDesktopClosed = menu.classList.contains('inactive')
+
+    if(!isMenuDesktopClosed) {
+        menu.classList.add('inactive')
+    }
+    
+     if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive');
+
+
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Compu',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Phone',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProducts(arr) {
+    for(product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.textContent = '$' + product.price
+        const productName = document.createElement('p');
+        productName.textContent = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+    
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(img, productInfo);
+    
+        
+        cardsContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productList);
